49ce47900d4eb31ef4f688c3bebf58fe41f4c288
izarojas99 <izareth.rojas@est.uca.edu.ni>
2020-10-09 14:28:16 -0700

    API

---
+++ API_KNX/Controllers/ArtistasController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_KNX.Data;
+using API_KNX.Models;
+
+namespace API_KNX.Controllers
+{
+    [Route("API_KNX/[controller]")]
+    [ApiController]
+    public class ArtistasController : ControllerBase
+    {
+        private readonly API_KNXContext _context;
+
+        public ArtistasController(API_KNXContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Artistas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Artista>>> GetArtista()
+        {
+            return await _context.Artista.ToListAsync();
+        }
+
+        // GET: api/Artistas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Artista>> GetArtista(int id)
+        {
+            var artista = await _context.Artista.FindAsync(id);
+
+            if (artista == null)
+            {
+                return NotFound();
+            }
+
+            return artista;
+        }
+
+        // PUT: api/Artistas/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArtista(int id, Artista artista)
+        {
+            if (id != artista.ArtistaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(artista).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArtistaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Artistas
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Artista>> PostArtista(Artista artista)
+        {
+            _context.Artista.Add(artista);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArtista", new { id = artista.ArtistaId }, artista);
+        }
+
+        // DELETE: api/Artistas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Artista>> DeleteArtista(int id)
+        {
+            var artista = await _context.Artista.FindAsync(id);
+            if (artista == null)
+            {
+                return NotFound();
+            }
+
+            _context.Artista.Remove(artista);
+            await _context.SaveChangesAsync();
+
+            return artista;
+        }
+
+        private bool ArtistaExists(int id)
+        {
+            return _context.Artista.Any(e => e.ArtistaId == id);
+        }
+    }
+}
---
+++ API_KNX/Data/API_KNXContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API_KNX.Models;
+
+namespace API_KNX.Data
+{
+    public class API_KNXContext : DbContext
+    {
+        public API_KNXContext (DbContextOptions<API_KNXContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<API_KNX.Models.Album> Album { get; set; }
+
+        public DbSet<API_KNX.Models.Artista> Artista { get; set; }
+    }
+}
